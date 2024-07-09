4c29845ab0afa2aab742eba70ee59d742521e0c0
Annah03 <108891973+Annah03@users.noreply.github.com>
2024-07-09 13:06:46 -0400

    Create MHZ19_working.ino
    Code from https://forum.arduino.cc/t/esp32-mh-z19b-co2-sensor/1168742/22. Modified to use ESPSoftwareSerial

---
+++ Sensors/MHZ19/MHZ19_working/MHZ19_working.ino
@@ -0,0 +1,65 @@
+#include <SoftwareSerial.h>
+#include <Arduino.h>
+#include "MHZ19.h"
+
+
+#define RX_PIN 16                                          // Rx pin which the MHZ19 Tx pin is attached to
+#define TX_PIN 17                                         // Tx pin which the MHZ19 Rx pin is attached to
+#define BAUDRATE 9600                                      // Device to MH-Z19 Serial baudrate (should not be changed)
+
+MHZ19 myMHZ19;                                             // Constructor for library
+/*
+#if defined(ESP32)
+HardwareSerial mySerial(2);                                // On ESP32 we do not require the SoftwareSerial library, since we have 2 USARTS available
+#else
+#include <SoftwareSerial.h>                                //  Remove if using # or non-uno compatible device
+*/
+EspSoftwareSerial::UART mySerial(RX_PIN, TX_PIN);                   // (Uno example) create device to MH-Z19 serial
+//#endif
+
+/*
+#define MYPORT_TX 16
+#define MYPORT_RX 17
+*/
+
+
+unsigned long getDataTimer = 0;
+
+void setup()
+{
+    Serial.begin(9600);                                     // Device to serial monitor feedback
+
+  
+    mySerial.begin(BAUDRATE);                               // (Uno example) device to MH-Z19 serial start
+    myMHZ19.begin(mySerial);                                // *Serial(Stream) reference must be passed to library begin().
+    
+    /*
+    myPort.begin(BAUDRATE);
+    myMHZ19.begin(myPort);
+    */
+    myMHZ19.autoCalibration();                              // Turn auto calibration ON (OFF autoCalibration(false))
+}
+
+void loop()
+{
+    if (millis() - getDataTimer >= 2000)
+    {
+        int CO2;
+
+        /* note: getCO2() default is command "CO2 Unlimited". This returns the correct CO2 reading even
+        if below background CO2 levels or above range (useful to validate sensor). You can use the
+        usual documented command with getCO2(false) */
+
+        CO2 = myMHZ19.getCO2();                             // Request CO2 (as ppm)
+
+        Serial.print("CO2 (ppm): ");
+        Serial.println(CO2);
+
+        int8_t Temp;
+        Temp = myMHZ19.getTemperature();                     // Request Temperature (as Celsius)
+        Serial.print("Temperature (C): ");
+        Serial.println(Temp);
+
+        getDataTimer = millis();
+    }
+}
